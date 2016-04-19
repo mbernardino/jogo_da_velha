56f4fdc6af9d7aa7dc7e9bbc30c8fc617a2f4509
Moisés Bernardino <bernardinomoises@gmail.com>
2016-04-19 20:29:54 -0300

    POC inicial: 01 Led + 01 Botão

--- PROJETO_JOGO_DA_VELHA_0_1/PROJETO_JOGO_DA_VELHA_0_1.ino
+++ PROJETO_JOGO_DA_VELHA_0_1/PROJETO_JOGO_DA_VELHA_0_1.ino
@@ -2,13 +2,23 @@
 
 // etapa 01: ligar um LED usando um botão
 
+  const int LED_00 = 13;
+  const int BTN_00 = 2;
+  int status_btn_00 = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(LED_00, OUTPUT);
+  pinMode(BTN_00, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  status_btn_00 = digitalRead(BTN_00);
+  if(status_btn_00 == HIGH){
+      digitalWrite(LED_00, HIGH);
+    } else {
+      digitalWrite(LED_00, LOW);
+    } 
 }
+
+
+

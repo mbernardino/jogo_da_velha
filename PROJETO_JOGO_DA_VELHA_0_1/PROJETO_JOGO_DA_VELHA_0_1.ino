e994f067ab864c590890cccc8662558ffda6b2bc
Moisés Bernardino <bernardinomoises@gmail.com>
2016-04-19 20:14:13 -0300

    estrutura inicial

---
+++ PROJETO_JOGO_DA_VELHA_0_1/PROJETO_JOGO_DA_VELHA_0_1.ino
@@ -0,0 +1,14 @@
+/* PROJETO: JOGO DA VELHA*/
+
+// etapa 01: ligar um LED usando um botão
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

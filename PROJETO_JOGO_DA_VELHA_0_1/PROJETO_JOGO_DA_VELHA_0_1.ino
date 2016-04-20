c4614b5284c1e481b32c122680e5a8dcb397ac48
Moisés Bernardino <bernardinomoises@gmail.com>
2016-04-19 21:33:07 -0300

    POC02: adicionada = 02 botões + 02 LEDs

--- PROJETO_JOGO_DA_VELHA_0_1/PROJETO_JOGO_DA_VELHA_0_1.ino
+++ PROJETO_JOGO_DA_VELHA_0_1/PROJETO_JOGO_DA_VELHA_0_1.ino
@@ -1,23 +1,36 @@
 /* PROJETO: JOGO DA VELHA*/
 
-// etapa 01: ligar um LED usando um botão
-
   const int LED_00 = 13;
+  const int LED_01 = 12;
   const int BTN_00 = 2;
+  const int BTN_01 = 3;
   int status_btn_00 = 0;
+  int status_btn_01 = 0;
 
 void setup() {
   pinMode(LED_00, OUTPUT);
   pinMode(BTN_00, INPUT);
+  pinMode(LED_01, OUTPUT);
+  pinMode(BTN_01, INPUT);
 }
 
 void loop() {
+  
   status_btn_00 = digitalRead(BTN_00);
-  if(status_btn_00 == HIGH){
+  status_btn_01 = digitalRead(BTN_01);
+  
+    if(status_btn_00 == HIGH){
       digitalWrite(LED_00, HIGH);
     } else {
       digitalWrite(LED_00, LOW);
-    } 
+    }
+
+   if(status_btn_01 == HIGH){
+     digitalWrite(LED_01, HIGH);  
+   } else {
+     digitalWrite(LED_01, LOW); 
+   }
+   
 }
 
 
